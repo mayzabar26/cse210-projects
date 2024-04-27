1e9318548a1632f9cfc42c9308f219f46681b4c1
mayzabar26 <mayzabar26@gmail.com>
2024-04-27 17:42:49 -0300

    C# Prep 1—Variables, Input, and Output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Asking for first name and last name
         Console.WriteLine("Hello Prep1 World!");
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
@@ -12,6 +13,7 @@ class Program
         string last = Console.ReadLine();
         Console.WriteLine();
 
+        //Sentence's output 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
         Console.WriteLine();
     }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+       
+        Console.Write("What is your guess? ");
+        string guessNumber = Console.ReadLine();
+
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+       
+
     }
 }
