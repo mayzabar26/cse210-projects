6a5e7847095f77cb7a425bc8fdd0c001e6c7f648
mayzabar26 <mayzabar26@gmail.com>
2024-04-27 18:32:38 -0300

    C# Prep 3 - Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,16 +6,34 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
 
-        Console.Write("What is the magic number? ");
-        string magicNumber = Console.ReadLine();
-       
-        Console.Write("What is your guess? ");
-        string guessNumber = Console.ReadLine();
+        //Asking for the magic number and the user's guess number
+        Console.Write("What is your magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
 
-
+        //Random number generator
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 101);
-       
+        magicNumber = randomGenerator.Next(1, 101);
 
+        int guess = -1;
+
+        //While Loops here
+        while (guess != magicNumber)
+        {
+            Console.Write("Whats is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
-}
+}        
