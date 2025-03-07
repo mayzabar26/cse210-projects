023696c059e01392f73ff98c4372d43ee329b341
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-03-06 22:15:31 -0300

    C# Programming Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Asking the user about their score
+        Console.Write("What is your grade percentage? ");
+        string score = Console.ReadLine();
+
+        //Using tryparse to motify the string to a number
+        if (int.TryParse(score, out int number))
+        {
+
+            //Condition
+            if (number >= 90)
+            {
+                Console.WriteLine("Congrats. You got an A!");
+            }
+            else if (number >= 80)
+            {
+                Console.WriteLine("Congrats. You got an B!");
+            }
+            else if (number >= 70)
+            {
+                Console.WriteLine("Congrats. You got a C!");
+            }
+            else if (number >= 60)
+            {
+                Console.WriteLine("You passed, but you need to improve. You got a D.");
+            }
+            else
+            {
+                Console.WriteLine("You didn't pass. Better luck next time.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
     }
 }
