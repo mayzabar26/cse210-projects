e3ee9a3da95b02ab3ba43d466fb2000766ef6db1
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-03-08 16:35:30 -0300

    C# Programming Exercise 5: Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+
+    }
+
+    //First function: Welcome Function
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    //Second function: User's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    //Third function: Favorite Number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    //Fourth function: Square Number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    //Fifth function: Result
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.Write($"{name}, the square of your number is {squaredNumber}");
     }
 }
