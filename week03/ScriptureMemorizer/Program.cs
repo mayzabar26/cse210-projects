099c1f92d9000ec631f1185b95a990f50d68241e
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-03-19 18:46:01 -0300

    W03 Team Activity: Scripture Memorizer Design

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.WriteLine("\n================SCRIPTURE================");
+        Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own un understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        Console.WriteLine("===========================================");
+
+        Console.Write("\nPress enter to continue or type 'quit' to finish: ");
+
     }
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -6,4 +6,44 @@ public class Word
     private string _text;
     private bool _isHidden;
 
+
+    //Constructor: Starts the verse with all the words visible
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    //Method 01: Hides the word
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+
+    //Method 02: Shows the word
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+
+    //Method 03: Check if the word is hidden
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+
+    //Method 04:
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+            return new string('_', _text.Length); //Word is be replaced by '_'
+        else
+            return _text;
+    }
+
+
+
 }
