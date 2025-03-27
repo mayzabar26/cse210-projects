4cdf0b670d4893f4f798fb4190947102ca6bc034
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-03-27 18:58:16 -0300

    Adding Video Class

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Comment
+{
+    //Setting the variables
+    private string _commenter;
+    private string _text;
+
+
+    //Setting constructors
+    public Comment(string commenter, string text)
+    {
+        _commenter = commenter;
+        _text = text;
+    }    
+
+
+    //Setting method: display_comment()
+    public void display_comment()
+    {
+        
+    }
+}
+
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+class Video
+{
+    //Setting the variables
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments; 
+
+
+    //Setting constructors
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
+    }
+
+
+    //Setting method: GetCommentCount()
+    //Will count the amount of comments
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+
+    //Setting method: AddComment()
+    //Will add a comment to the video
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+
+    //Setting method: display_info()
+    public void display_info()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_length}");
+        Console.WriteLine($"Number of comments: {GetCommentCount()}");
+        Console.WriteLine($"Comments: ");
+
+        foreach(Comment comment in _comments)
+        {
+            comment.display_comment();
+        }
+        Console.WriteLine();
+    }
+}
